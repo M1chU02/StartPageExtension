f89bc65ae3448d28223959f13cf136222c4c3cda
M1chU02 <m.mikolajewski02@gmail.com>
2025-12-20 22:37:07 +0100

    Fixed almost all socials svgs misalignments.

--- script/socials.js
+++ script/socials.js
@@ -4,72 +4,84 @@ const SOCIAL_SITES = {
     iconClass: "spotify",
     svgHref: "/svg/socialsvg/spotify.svg#spotify",
     backgroundColor: "#1DB954",
+    viewBox: "0 0 48 48",
   },
   Facebook: {
     url: "https://www.facebook.com/",
     iconClass: "facebook",
     svgHref: "/svg/socialsvg/facebook.svg#facebook",
     backgroundColor: "#1877F2",
+    viewBox: "0 0 48 48",
   },
   Instagram: {
     url: "https://www.instagram.com/",
     iconClass: "instagram",
     svgHref: "/svg/socialsvg/instagram.svg#instagram",
     backgroundColor: "#C13584",
+    viewBox: "0 0 48 48",
   },
   Twitter: {
     url: "https://twitter.com/",
     iconClass: "twitter",
     svgHref: "/svg/socialsvg/twitter.svg#twitter",
     backgroundColor: "#000000",
+    viewBox: "0 0 24 24",
   },
   Linkedin: {
     url: "https://www.linkedin.com/",
     iconClass: "linkedin",
     svgHref: "/svg/socialsvg/linkedin.svg#linkedin",
     backgroundColor: "#0A66C2",
+    viewBox: "0 0 48 48",
   },
   Youtube: {
     url: "https://www.youtube.com/",
     iconClass: "youtube",
     svgHref: "/svg/socialsvg/youtube.svg#youtube",
     backgroundColor: "#FF0000",
+    viewBox: "0 0 48 48",
   },
   Github: {
     url: "https://github.com/",
     iconClass: "github",
     svgHref: "/svg/socialsvg/github.svg#github",
     backgroundColor: "#24292e",
+    viewBox: "0 0 48 48",
   },
   TikTok: {
     url: "https://www.tiktok.com/",
     iconClass: "tiktok",
     svgHref: "/svg/socialsvg/tiktok.svg#tiktok",
     backgroundColor: "#000000",
+    viewBox: "0 0 48 48",
   },
   Reddit: {
     url: "https://www.reddit.com/",
     iconClass: "reddit",
     svgHref: "/svg/socialsvg/reddit.svg#reddit",
     backgroundColor: "#FF4500",
+    viewBox: "0 0 48 48",
   },
   Discord: {
     url: "https://discord.com/",
     iconClass: "discord",
     svgHref: "/svg/socialsvg/discord.svg#discord",
     backgroundColor: "#5865F2",
+    viewBox: "0 0 24 24",
   },
   Messenger: {
     url: "https://www.messenger.com/",
     iconClass: "messenger",
     svgHref: "/svg/socialsvg/messenger.svg#messenger",
     backgroundColor: "#0084FF",
+    viewBox: "0 0 32 32",
   },
   Notion: {
     url: "https://www.notion.so/",
     iconClass: "notion",
     svgHref: "/svg/socialsvg/notion.svg#notion",
     backgroundColor: "#000000",
+    viewBox: "0 0 15 15",
   },
 };
 
@@ -101,6 +113,9 @@ function applySocialToElement(btn, cardId) {
   const svg = btn.querySelector("svg");
   if (svg) {
     svg.setAttribute("class", config.iconClass);
+    if (config.viewBox) {
+      svg.setAttribute("viewBox", config.viewBox);
+    }
     const use = svg.querySelector("use");
     if (use) {
       use.setAttributeNS(
